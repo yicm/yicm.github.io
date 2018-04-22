6b91cf57c68d21bc271eb46e486d4e8c8dd40ac8
yicm <ycm_hy@163.com>
2018-04-22 16:15:20 +0800

    Add back_to_top button

--- _includes/scripts/post.js
+++ _includes/scripts/post.js
@@ -4,6 +4,34 @@
   var TOC_SELECTOR = window.TEXT_VARIABLES.site.toc.selectors;
   var LEANCLOUD = window.TEXT_VARIABLES.site.leancloud;
   window.Lazyload.js(SOURCES.jquery, function() {
+    // ----------------  
+    // back to top
+	// When to show the scroll link
+	// higher number = scroll link appears further down the page   
+	var upperLimit = 500;
+	
+	// Our scroll link element
+	var scrollElem = $('#totop');
+    var scrollElemA = $('#totop_a');
+   
+	// Scroll to top speed
+	var scrollSpeed = 500;
+   
+	// Show and hide the scroll to top link based on scroll position   
+	scrollElem.hide();
+	$(window).scroll(function () {            
+		var scrollTop = $(document).scrollTop();       
+		if ( scrollTop > upperLimit ) {
+			$(scrollElem).stop().fadeTo(300, 1); // fade back in           
+		}else{       
+			$(scrollElem).stop().fadeTo(300, 0); // fade out
+		}
+	});
+	// Scroll to top animation on click
+	$(scrollElem).click(function(){
+		$('html, body').animate({scrollTop:0}, scrollSpeed); return false;
+	});   
+    // ----------------
     var $window = $(window);
     var $pageStage = $('.js-page-stage');
     var $pageFooter = $('.js-page-footer');
